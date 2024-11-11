d83c13c1afa014bdb98823023d7b749773957e1b
prolley-parnell <prolleyp@gmail.com>
2024-11-11 13:47:27 +0000

    Debug install scripts

--- single_job.sh
+++ single_job.sh
@@ -1,10 +1,10 @@
 file_name=$1 #YYMMDD-HHMM_camX_fruit_sessionY
 scratch_input=$2 #src_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/input
 scratch_output=$3 #dest_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/output
-
+mkdir -p "${scratch_input}/${file_name}"
 tar -xf "${scratch_input}/${file_name}.tar" -C "${scratch_input}/${file_name}"
 
-python main.py \
+python segment.py \
       --video_dir="${scratch_input}/${file_name}" \
       --model_cfg="${scratch_input}/configs/sam2.1/sam2.1_hiera_l.yaml" \
       --checkpoint="${scratch_input}/checkpoints/sam2.1_hiera_large.pt" \
