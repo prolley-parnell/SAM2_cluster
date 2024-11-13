fe37bb770131cfde0db9cd9183597f49e2dab07f
prolley-parnell <prolleyp@gmail.com>
2024-11-13 16:59:50 +0000

    Updated batch slurm params

--- array_job.sh
+++ array_job.sh
@@ -39,10 +39,10 @@
 #SBATCH --nodes=1
 
 # Generic resources to use - typically you'll want gpu:n to get n gpus
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
-#SBATCH --mem=20000
+#SBATCH --mem=80000
 
 # Number of CPUs to use. Check `cluster-status` for node configurations
 #SBATCH --cpus-per-task=4
