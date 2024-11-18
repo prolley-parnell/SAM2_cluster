92325972e3339309e03f027866558b6e91be1f4b
prolley-parnell <prolleyp@gmail.com>
2024-11-18 17:49:08 +0000

    update slurm params

--- array_job.sh
+++ array_job.sh
@@ -46,7 +46,7 @@
 #SBATCH --mem=23000
 
 # Number of CPUs to use. Check `cluster-status` for node configurations
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 
 # Maximum time for the job to run, format: days-hours:minutes:seconds
 #SBATCH --time=1-04:00:00
