3f9716219b8c12dbe8a7a2df414fc428ca93bd36
prolley-parnell <prolley-parnell@gmail.com>
2025-01-07 17:07:40 +0000

    update and include cleanup

---
+++ clean_up.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+PROJECT_NAME=segment
+project_path=/home/${USER}/${PROJECT_NAME}
+
+echo "You are cleaning up, this will delete any input and output files left in the dfs after running rsync"
+echo "Do you wish to continue? [y/n]"
+
+read response < /dev/tty
+if [ ! "$response" = "y" ] ; then
+  exit
+fi
+
+dfs_output_path="${project_path}/data/output"
+afs_output_path="/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/output"
+
+
+#Make the data output path if it is not present
+if [ ! -d ${afs_output_path} ]; then
+  mkdir -p ${afs_output_path}
+fi
+
+#Synchronise the folders and move the AFS input to DFS input
+rsync --archive --update --compress --progress ${dfs_output_path}/ ${afs_output_path}
+echo "${dfs_output_path}/ up to date with ${afs_output_path}"
+
+rm -rf ${dfs_output_path}
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #USER=$1 #sXXXXXXXX
 PROJECT_NAME=segment
-afs_src=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/input #s20 is the first two digits of the student number given in $USER
+afs_input_path=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/input #s20 is the first two digits of the student number given in $USER
 
 echo "You are on branch dfs so are setting up the DFS."
 echo "Press Ctrl+C if this is not correct and switch to the correct branch."
@@ -16,33 +16,33 @@ if [ ! -d "${conda_path}" ]; then
   conda config --set auto_activate_base false
 fi
 
-project_path=/home/${USER}/${PROJECT_NAME}
+dfs_project_path=/home/${USER}/${PROJECT_NAME}
 
 #Make the project path if it is not present
-if [ ! -d "${project_path}" ]; then
-  mkdir -p ${project_path}
+if [ ! -d "${dfs_project_path}" ]; then
+  mkdir -p ${dfs_project_path}
 fi
 
-dfs_dst="${project_path}/data/input"
+dfs_input_path="${dfs_project_path}/data/input"
 
 #Make the data input path if it is not present
-if [ ! -d ${dfs_dst} ]; then
-  mkdir -p ${dfs_dst}
+if [ ! -d ${dfs_input_path} ]; then
+  mkdir -p ${dfs_input_path}
 fi
 
 #Synchronise the folders and move the AFS input to DFS input
-rsync --archive --update --compress --progress ${afs_src}/ ${dfs_dst}
+rsync --archive --update --compress --progress ${afs_input_path}/ ${dfs_input_path}
 
-echo "${afs_src}/ up to date with ${dfs_dst}"
+echo "${afs_input_path}/ up to date with ${dfs_input_path}"
 
 #Clone the SAM2 repo -if it is not already present
-if [ ! -d "${project_path}/sam2" ]; then
-  cd ${project_path} || echo "Could not enter folder ${project_path}" && exit
+if [ ! -d "${dfs_project_path}/sam2" ]; then
+  cd ${dfs_project_path} || echo "Could not enter folder ${dfs_project_path}" && exit
   git clone https://github.com/facebookresearch/sam2.git
   conda create -n sam2 python=3.10
   conda activate sam2
   #To resolve issues with downloading packages to a /tmp folder that is not big enough
-  TMPDIR="${project_path}/tmp"
+  TMPDIR="${dfs_project_path}/tmp"
   TMP="${TMPDIR}"
   TEMP="${TMPDIR}"
   mkdir -p "${TMPDIR}"
@@ -50,22 +50,23 @@ if [ ! -d "${project_path}/sam2" ]; then
   cd sam2
   pip install -e .
   pip install pycocotools
-  conda env config vars set SAM2_HOME="${project_path}/sam2"
+  conda env config vars set SAM2_HOME="${dfs_project_path}/sam2"
   conda deactivate
+  rm -rf ${TMPDIR}
 fi
 
 #Download the SAM2 checkpoints if checkpoints folder does not already exist - Edited because it does not need to be imported with new data
-if [ ! -f "${project_path}/sam2/checkpoints/sam2.1_hiera_base_plus.pt" ]; then
+if [ ! -f "${dfs_project_path}/sam2/checkpoints/sam2.1_hiera_base_plus.pt" ]; then
   #Run any installation commands
-  mkdir -p "${project_path}/sam2/checkpoints"
-#  wget -P "${project_path}/sam2/checkpoints" https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_large.pt
-  wget -P "${project_path}/sam2/checkpoints" https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_base_plus.pt
+  mkdir -p "${dfs_project_path}/sam2/checkpoints"
+  wget -P "${dfs_project_path}/sam2/checkpoints" https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_base_plus.pt
 fi
 
 #Ensure that you can use the "run_experiments" wrapper
 #echo 'export PATH=/home/$USER/cluster-scripts/experiments:$PATH' >> ~/.bashrc
-#if [ -f "${dfs_dst}/input.tar.bz2" ]; then
-#  tar --exclude="._*" -xjvf "${dfs_dst}/input.tar.bz2" -C "${dfs_dst}/"
-#else
-#  echo "Could not find '${dfs_dst}/input.tar.bz2'"
-#fi
+if [ -f "${dfs_input_path}/input.tar.bz2" ]; then
+  tar --exclude="._*" -xjvf "${dfs_input_path}/input.tar.bz2" -C "${dfs_project_path}/data"
+  rm -rf "${dfs_input_path}/input.tar.bz2"
+else
+  echo "Could not find '${dfs_input_path}/input.tar.bz2'"
+fi
