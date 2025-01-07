80fe325c6640882d3098ef1fe747b619f149b486
prolley-parnell <prolley-parnell@gmail.com>
2025-01-07 16:49:59 +0000

    update and include cleanup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ input_folder=$2 #Path to the /data folder that contains an input directory that 
 PROJECT_NAME=segment
 afs_project_path=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME} #The s20 is the first two numbers of the user student number
 
-echo "You are on branch `afs` so are setting up the AFS."
+echo "You are on branch afs so are setting up the AFS."
 echo "Press Ctrl+C if this is not correct and switch to the correct branch."
 
 
@@ -14,15 +14,15 @@ if [ ! -d "${afs_project_path}" ]; then
 fi
 
 #If there is no data/input folder in the project folder, then make it.
-afs_src="${afs_project_path}/data/input"
+afs_dst="${afs_project_path}/data/input"
 
-if [ ! -d ${afs_src} ]; then
-  mkdir -p ${afs_src}
+if [ ! -d ${afs_dst} ]; then
+  mkdir -p ${afs_dst}
 fi
 
 if [ ! -f "${input_folder}/input.tar.bz2" ]; then
   cd ${input_folder}
-  tar --no-xattrs --exclude="._*" -cjf input.tar.bz2 input
-  mv input.tar.bz2 ${afs_src}
+  tar --no-xattrs --exclude="._*" -cjvf input.tar.bz2 input
+  mv input.tar.bz2 ${afs_dst}
 fi
 
