7819a8c9985e8803bc7ded278d1bbd64200c0535
prolley-parnell <prolleyp@gmail.com>
2025-01-09 16:05:41 +0000

    revert to large checkpoint

--- single_job.sh
+++ single_job.sh
@@ -2,12 +2,12 @@ file_name=$1 #YYMMDD-HHMM_camX_fruit_sessionY
 scratch_input=$2 #src_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/input
 scratch_output=$3 #dest_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/output
 mkdir -p "${scratch_input}/${file_name}"
-tar --exclude="._*" -xjf "${scratch_input}/${file_name}.tar.bz2" -C "${scratch_input}/"
+tar --exclude="._*" -xjvf "${scratch_input}/${file_name}.tar.bz2" -C "${scratch_input}/"
 
 python segment.py \
       --video_dir="${scratch_input}/${file_name}" \
-      --model_cfg="configs/sam2.1/sam2.1_hiera_b+.yaml" \
-      --checkpoint="${SAM2_HOME}/checkpoints/sam2.1_hiera_base_plus.pt" \
+      --model_cfg="configs/sam2.1/sam2.1_hiera_l.yaml" \
+      --checkpoint="${SAM2_HOME}/checkpoints/sam2.1_hiera_large.pt" \
       --annotation="${scratch_input}/${file_name}/${file_name}.csv" \
       --experiment_name="${file_name}" \
       --output_destination="${scratch_output}"
