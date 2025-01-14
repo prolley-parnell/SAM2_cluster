eb9aaa1d87fdd5c11367a1f3a991591b8f17a7f1
prolley-parnell <prolley-parnell@gmail.com>
2025-01-14 17:42:08 +0000

    update setup to allow for file naming

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #USER=$1 #sXXXXXXXX
 PROJECT_NAME=segment
+set_name=$1
 
 echo "You are on branch scratch so are setting up the DFS for use with scratch."
 echo "This unzips an input tar but this is not necessary for some scripts. It is for training."
@@ -9,12 +10,21 @@ echo "Press Ctrl+C if this is not correct and switch to the correct branch."
 project_path=/home/${USER}/${PROJECT_NAME}
 dfs_input_path="${project_path}/data/input"
 
+if [ $# -eq 0 ]; then
+  echo "Set name has been left blank, using input.tar.bz2"
+  if [ -f "${dfs_input_path}/input.tar.bz2" ]; then
+    tar --exclude="._*" -xjvf "${dfs_input_path}/input.tar.bz2" -C "${dfs_input_path}/.."
+    rm -rf "${dfs_input_path}/input.tar.bz2"
+  else
+    echo "Could not find '${dfs_input_path}/input.tar.bz2'"
+  fi
 
-#Ensure that you can use the "run_experiments" wrapper
-#echo 'export PATH=/home/$USER/cluster-scripts/experiments:$PATH' >> ~/.bashrc
-if [ -f "${dfs_input_path}/input.tar.bz2" ]; then
-  tar --exclude="._*" -xjvf "${dfs_input_path}/input.tar.bz2" -C "${dfs_input_path}/.."
-  rm -rf "${dfs_input_path}/input.tar.bz2"
 else
-  echo "Could not find '${dfs_input_path}/input.tar.bz2'"
+  echo "Set name is: ${set_name}"
+  if [ -f "${dfs_input_path}/input/${set_name}.tar.bz2" ]; then
+    tar --exclude="._*" -xjvf "${dfs_input_path}/input/${set_name}.tar.bz2" -C "${dfs_input_path}/input"
+    rm -rf "${dfs_input_path}/input/${set_name}.tar.bz2"
+  else
+    echo "Could not find '${dfs_input_path}/input.tar.bz2'"
+  fi
 fi
