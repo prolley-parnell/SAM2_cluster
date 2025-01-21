c62bf4b35d126c0ce50f038793bdc3acebe8fa25
prolley-parnell <prolley-parnell@gmail.com>
2025-01-21 15:13:23 +0000

    update memory limits based on discussion

--- set_array_job/array_job_set_4.sh
+++ set_array_job/array_job_set_4.sh
@@ -42,8 +42,8 @@
 #SBATCH --gres=gpu:1
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
-# NODE_MAX_MEM - 1000 / N_GPU = (190000 - 1000) / 8
-#SBATCH --mem=23000
+# NODE_MAX_MEM - 1000 / N_GPU = (515000 - 1000) / 4
+#SBATCH --mem=128750
 
 # Number of CPUs to use. Check `cluster-status` for node configurations
 #SBATCH --cpus-per-task=1
